c3b8236ec3a92bb883adec5e1dd9539dc137320a
Fernanda Kipper <nanda.kipper@gmail.com>
2022-05-10 19:06:11 -0300

    feat: initial template

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { faker } from '@faker-js/faker';
+
+
+const generateCards = (totalCards: number) => {
+  let newCards = []
+  for (let i = 0; i < totalCards; i++) {
+    newCards.push(faker.name.firstName())
+  }
+  return newCards;
+}
 
 function App() {
+  const [cards, setCards] = useState(() => {
+    return generateCards(8)
+  })
+
+  const loadMoreCards = () => {
+    const newCards = generateCards(4)
+    setCards(cards => [...cards, ...newCards])
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </header>
+    <div className="wrap">
+      <div className="cards-container">
+        {cards.map(card => <div className="card">{card}</div>)}
+      </div>
+      <button onClick={loadMoreCards}>carregar mais cards</button>
     </div>
   );
 }
